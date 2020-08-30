77ccbc01f087c05fecd19f3a2272885088ff6e93
pcreem <creempepper@gmail.com>
2020-08-31 01:06:59 +0800

    simplify (unfinished)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Combine from './Post'
+import Post from './Post'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div>
-      <Combine />
+      <Post />
     </div>
   )
 }
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Toast, Modal, Form, Button } from 'react-bootstrap';
+// import { ListGroup, Modal } from 'react-bootstrap';
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from 'urql';
 
@@ -8,143 +8,99 @@ const FEED_QUERY = gql`
     info(authorId:"1"){
         id
         title
-        content
-        createdAt
       }
   }
 `
 
 const UPSERT_POST = gql`
-  mutation upsertPost($authorId: ID!, $postId:ID!, $title: String!, $content: String) {
-    upsertPost(authorId:$authorId, postId: $postId, title: $title, content: $content) {
+  mutation upsertPost($authorId: ID!, $postId:ID!, $title: String!) {
+    upsertPost(authorId:$authorId, postId: $postId, title: $title) {
       id
-      content
       title
     }
   }
 `
 
-const DELETE_POST = gql`
-  mutation deletePost($postId:ID!) {
-    deletePost(postId: $postId) {
-      id
-    }
-  }
-`
+// const DELETE_POST = gql`
+//   mutation deletePost($postId:ID!) {
+//     deletePost(postId: $postId) {
+//       id
+//     }
+//   }
+// `
 
-function refreshPage() {
-  window.location.reload(false);
-}
+// function refreshPage() {
+//   window.location.reload(false);
+// }
 
-
-function PostForm() {
-  //Upsert Section
-  let postId = 0
+const Post = () => {
   const authorId = "1"
-  const [content, setContent] = React.useState('')
+  const postId = "1"
+  //Upsert Section
   const [title, setTitle] = React.useState('')
-
   const [upsertState, executeUpsert] = useMutation(UPSERT_POST)
   const upsert = React.useCallback(() => {
-    if (title.length !== 0 || content.length !== 0) { executeUpsert({ authorId, postId, title, content }) }
-  }, [executeUpsert, authorId, postId, title, content])
+    if (title.length !== 0) { executeUpsert({ authorId, postId, title }) }
+  }, [executeUpsert, authorId, postId, title])
 
 
-  return (
-    <Form >
-      <Form.Group>
-        <Form.Control type="text" placeholder={'title...'} value={title}
-          onChange={e => setTitle(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Control as="textarea" placeholder={'content...'} rows="3" value={content}
-          onChange={e => setContent(e.target.value)} />
-      </Form.Group>
-      <Button variant="outline-dark" disabled={upsertState.fetching} onClick={() => { upsert(); refreshPage(); }} >Finish</Button>
-    </Form>
-  )
-}
 
-const Post = ({ post }) => {
   //Delete Section
-  const postId = post.id
-  const authorId = "1"
+  // const [postId, setPostId] = React.useState('')
+  // function setPId(postid) {
+  //   return setPostId(postid)
+  // }
 
-  const [showA, setShowA] = React.useState(true);
-  const toggleShowA = () => setShowA(!showA);
-  const [showB, setShowB] = React.useState(false);
-  const toggleShowB = () => setShowB(!showB);
+  // const [showA, setShowA] = React.useState(true);
+  // const toggleShowA = () => setShowA(!showA);
+  // const [showB, setShowB] = React.useState(false);
+  // const toggleShowB = () => setShowB(!showB);
 
-  const [deleteState, executeDelete] = useMutation(DELETE_POST)
-  const del = React.useCallback(() => {
-    executeDelete({ postId })
-  }, [executeDelete, postId])
+  // const [deleteState, executeDelete] = useMutation(DELETE_POST)
+  // const del = React.useCallback(() => {
+  //   executeDelete({ postId })
+  // }, [executeDelete, postId])
 
-  //Upsert Section
-  const [content, setContent] = React.useState('')
-  const [title, setTitle] = React.useState('')
-
-  const [upsertState, executeUpsert] = useMutation(UPSERT_POST)
-  const upsert = React.useCallback(() => {
-    if (title.length !== 0 || content.length !== 0) { executeUpsert({ authorId, postId, title, content }) }
-  }, [executeUpsert, authorId, postId, title, content])
-
-  return (
-    <>
-      <Modal show={showB}>
-        <Modal.Body>
-          <Form >
-            <Form.Group>
-              <Form.Control type="text" placeholder={post.title} value={title}
-                onChange={e => setTitle(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" placeholder={post.content} rows="3" value={content}
-                onChange={e => setContent(e.target.value)} />
-            </Form.Group>
-            <Button variant="outline-dark" disabled={upsertState.fetching} onClick={() => { upsert(); toggleShowB(); }} >Finish</Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-
-
-      <Toast show={showA} disabled={deleteState.fetching} onClose={() => { del(); toggleShowA(); }}>
-        <Toast.Header >
-          <strong onClick={toggleShowB} className="mr-auto">{post.title}</strong>
-        </Toast.Header>
-        <Toast.Body onClick={toggleShowB}>{post.content}</Toast.Body>
-      </Toast>
-    </>
-  )
-}
-
-const PostList = () => {
   const [result] = useQuery({ query: FEED_QUERY })
   const { data, fetching, error } = result
 
   if (fetching) return <div>Fetching</div>
   if (error) return <div>Error</div>
 
-  const postsToRender = data.info
+  console.log(data.info)
 
-  return (
-    <div>
-      {postsToRender.map(post => <Post key={post.id} post={post} />)}
-    </div>
-  );
-};
-
-const Combine = () => {
   return (
     <>
-      <PostForm />
-      <hr></hr>
-      <PostList />
+      <form className="form-inline">
+        <input type="text" className="form-control" placeholder="add a new todo..." value={title}
+          onChange={e => setTitle(e.target.value)} />
+        <button type="submit" className="btn btn-default" disabled={upsertState.fetching} onClick={() => { upsert(); }}>Add</button>
+      </form>
+
+      {/* {postsToRender.map(post =>
+        <React.Fragment key={post.id}>
+          <Modal show={showB}>
+            <Modal.Body>
+              <form className="form-inline">
+                <input type="text" className="form-control" placeholder={post.title} value={title}
+                  onChange={e => setTitle(e.target.value.length === 0 ? post.title : e.target.value)} />
+                <button type="submit" className="btn btn-default" disabled={upsertState.fetching} onClick={() => { upsert(); setPId(post.id) }} >Update</button>
+              </form>
+            </Modal.Body>
+          </Modal>
+
+          <ListGroup>
+            <ListGroup.Item show={showA}>
+              <p onClick={toggleShowB} className="mr-auto">{post.title}</p>
+              <button type="button" disabled={deleteState.fetching} onClick={() => { setPId(post.id); del(); toggleShowA(); }}>&times;</button>
+            </ListGroup.Item>
+          </ListGroup>
+        </React.Fragment>
+      )} */}
     </>
   )
-
 }
 
 
 
-export default Combine
+export default Post
