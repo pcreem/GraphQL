c8efa492df9d43e6746ccfd6d2f9a9a17b4b870b
pcreem <creempepper@gmail.com>
2020-09-02 19:52:36 +0800

    setup Sign page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form, Button, Col, Row } from 'react-bootstrap';
 import gql from 'graphql-tag'
 import { useMutation } from 'urql'
 import { setToken } from '../token'
@@ -36,58 +37,50 @@ const Login = props => {
       .then(({ data }) => {
         const token = data && data[isLogin ? 'login' : 'signup'].token
         if (token) {
-          console.log("it's token")
+          console.log(data)
           setToken(token)
         }
       });
   }, [executeMutation, isLogin, email, password, name]);
 
   return (
-    <div>
-      <h4 className="mv3">{isLogin ? 'Login' : 'Sign Up'}</h4>
-
-      <div className="flex flex-column">
-        {!isLogin && (
-          <input
-            value={name}
-            onChange={e => setName(e.target.value)}
-            type="text"
-            placeholder="Your name"
-          />
-        )}
-        <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          type="text"
-          placeholder="Your email address"
-        />
-        <input
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          type="password"
-          placeholder="Choose a safe password"
-        />
-      </div>
-
-      <div className="flex mt3">
-        <button
-          type="button"
-          className="pointer mr2 button"
-          disabled={state.fetching}
-          onClick={mutate}
-        >
-          {isLogin ? "login" : "create account"}
-        </button>
-        <button
-          type="button"
-          className="pointer button"
-          disabled={state.fetching}
-          onClick={() => setIsLogin(!isLogin)}
-        >
-          {isLogin ? 'need to create an account?' : 'already have an account?'}
-        </button>
-      </div>
-    </div>
+    <>
+      <Row className="justify-content-md-center" style={{ marginTop: 100 }}>
+        <Col sm={6} >
+          <h4 className="text-center">{isLogin ? 'Sign In' : 'Sign Up'}</h4>
+          <br></br>
+          <Form>
+            {!isLogin && (
+              <Form.Group controlId="formBasicName">
+                <Form.Control
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  placeholder="Enter name" />
+              </Form.Group>
+            )}
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control value={email}
+                onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control value={password}
+                onChange={e => setPassword(e.target.value)} placeholder="Password" />
+            </Form.Group>
+            <Button variant="outline-secondary"
+              disabled={state.fetching}
+              onClick={() => { mutate(); }} type="submit">
+              {isLogin ? "login" : "create account"}
+            </Button>{' '}
+            <Button variant="outline-secondary"
+              disabled={state.fetching}
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              {isLogin ? 'need to create an account?' : 'already have an account?'}
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
   )
 }
 
