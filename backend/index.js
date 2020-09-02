3591d6c836a2e23ce9cc9438bb89e9e577d59511
pcreem <creempepper@gmail.com>
2020-09-03 03:08:26 +0800

    setup samesite cookie

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const { GraphQLServer } = require('graphql-yoga')
+const session = require("express-session");
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 
@@ -19,4 +20,22 @@ const server = new GraphQLServer({
     }
   },
 })
+
+const SESSION_SECRET = "lsdfjlkjlkewaqra";
+
+server.express.use(
+  session({
+    name: "qid",
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+    }
+  })
+);
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
--- frontend/src/token.js
+++ frontend/src/token.js
@@ -1,5 +1,6 @@
+import Cookies from 'js-cookie'
 const AUTH_TOKEN = 'auth-token';
 
-export const getToken = () => localStorage.getItem(AUTH_TOKEN);
-export const setToken = token => localStorage.setItem(AUTH_TOKEN, token);
-export const deleteToken = () => localStorage.removeItem(AUTH_TOKEN);
+export const getToken = () => Cookies.get(AUTH_TOKEN);
+export const setToken = token => Cookies.set(AUTH_TOKEN, token, { expires: 7 });
+export const deleteToken = () => Cookies.remove(AUTH_TOKEN);
