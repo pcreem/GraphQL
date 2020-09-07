b39f2dc9afbf542a664cf7575c3a47a7e9ab61a8
pcreem <creempepper@gmail.com>
2020-09-08 03:08:17 +0800

    fix cookie samesite/secure setting

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const { GraphQLServer } = require('graphql-yoga')
-const session = require("express-session");
 require('dotenv').config()
 const Query = require('./resolvers/query.js')
 const Mutation = require('./resolvers/mutation.js')
@@ -25,21 +24,6 @@ const server = new GraphQLServer({
     }
   },
 })
-
-server.express.use(
-  session({
-    name: "qid",
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: true,
-      sameSite: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-    }
-  })
-);
 
 const PORT = process.env.PORT || 4000
 server.start(PORT, () => console.log(`Server is running on http://localhost:4000`))
